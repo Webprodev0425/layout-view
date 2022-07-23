b0b42e8a0ba551cf8b41e0d184124149bc5c8b68
Webprodev0425 <newstar01@dreampotential.org>
2022-07-23 17:00:01 +0800

    draft

---
+++ data.js
@@ -0,0 +1,317 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance-view-main-store",
+      "name": "Entrance View Main Store",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.733736739999596,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.582144864132543,
+          "pitch": 0.20619108080155435,
+          "rotation": 0,
+          "target": "1-main-store-view"
+        },
+        {
+          "yaw": -3.0032763212866005,
+          "pitch": 0.37065799413524836,
+          "rotation": 4.71238898038469,
+          "target": "3-bottling-station-1"
+        },
+        {
+          "yaw": -2.731127984437668,
+          "pitch": 0.14749607875638482,
+          "rotation": 4.71238898038469,
+          "target": "4-bottling-stations-2--3"
+        },
+        {
+          "yaw": -2.577213396770226,
+          "pitch": 0.07640356450185948,
+          "rotation": 0,
+          "target": "5-fermentation-room"
+        },
+        {
+          "yaw": 1.0426978268584008,
+          "pitch": 0.4432771482767457,
+          "rotation": 4.71238898038469,
+          "target": "2-beercider-section"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-main-store-view",
+      "name": "Main Store View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.009471059999995646,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17676923601470662,
+          "pitch": 0.21315948972832643,
+          "rotation": 0,
+          "target": "0-entrance-view-main-store"
+        },
+        {
+          "yaw": 0.2696504350833653,
+          "pitch": 0.1087549098750884,
+          "rotation": 4.71238898038469,
+          "target": "2-beercider-section"
+        },
+        {
+          "yaw": 1.534333189253001,
+          "pitch": 0.284173818052885,
+          "rotation": 6.283185307179586,
+          "target": "3-bottling-station-1"
+        },
+        {
+          "yaw": 2.3031745375804222,
+          "pitch": 0.27088020674639424,
+          "rotation": 0,
+          "target": "4-bottling-stations-2--3"
+        },
+        {
+          "yaw": -2.8929015697148213,
+          "pitch": 0.4312089208095813,
+          "rotation": 0,
+          "target": "5-fermentation-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-beercider-section",
+      "name": "Beer/Cider/Mist Section",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.297085197867373,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4970533590919972,
+          "pitch": 0.4574738596486263,
+          "rotation": 4.71238898038469,
+          "target": "0-entrance-view-main-store"
+        },
+        {
+          "yaw": 1.2394929897436242,
+          "pitch": 0.1615981206668362,
+          "rotation": 0,
+          "target": "1-main-store-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bottling-station-1",
+      "name": "Bottling Station 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7920092750193319,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5350441654748908,
+          "pitch": -0.06931945061945832,
+          "rotation": 0,
+          "target": "4-bottling-stations-2--3"
+        },
+        {
+          "yaw": 3.13057744269466,
+          "pitch": 0.307060703253633,
+          "rotation": 0,
+          "target": "1-main-store-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bottling-stations-2--3",
+      "name": "Bottling Stations 2 & 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.1993915108145643,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.139254612504363,
+          "pitch": 0.21633820483431698,
+          "rotation": 0,
+          "target": "1-main-store-view"
+        },
+        {
+          "yaw": 1.4553412145879703,
+          "pitch": 0.1818417791867386,
+          "rotation": 4.71238898038469,
+          "target": "5-fermentation-room"
+        },
+        {
+          "yaw": -2.989929947408619,
+          "pitch": -3.556504424295781e-9,
+          "rotation": 0,
+          "target": "3-bottling-station-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-fermentation-room",
+      "name": "Fermentation Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.01521417718018,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.401124830474693,
+          "pitch": 0.22258740369550978,
+          "rotation": 0,
+          "target": "1-main-store-view"
+        },
+        {
+          "yaw": 2.455918176247966,
+          "pitch": 0.11866481121905181,
+          "rotation": 0,
+          "target": "0-entrance-view-main-store"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Barley Malt & Vine Store Virtual Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
